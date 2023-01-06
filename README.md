cdf5ba257a7a609aaba5eb2971edb7781a95e28f
Jonas A Sihlén <jonasasihlen@gmail.com>
2023-01-06 16:11:31 +0100

    Adding Member as entity class and updating README

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # DemoWebApp
+Practice project to learn how to use [Spring Boot](https://www.baeldung.com/spring-boot) for setting up a RESTful backend, and [Angular](https://angular.io/) to create a JavaScript frontend.
+
+# Spring Boot
+The demo web app's functionality is simplistic. The functionalities include:
+
+- Fetch and display a list of JPA entities
+- Calling an in-memory H2 database
+- Persist new through a simple HTML form
+
+# Maven Dependencies
+
+As you can see in the [pom.xml](https://github.com/nimblegeek/DemoWebApp/blob/5077bbd7351b0fa3b5a82267d8059385b50d148f/pom.xml) I am using thre main dependencies like so:
+
+```
+<dependency> 
+    <groupId>org.springframework.boot</groupId> 
+    <artifactId>spring-boot-starter-web</artifactId> 
+</dependency>
+<dependency> 
+    <groupId>org.springframework.boot</groupId> 
+    <artifactId>spring-boot-starter-data-jpa</artifactId> 
+</dependency>
+<dependency>
+    <groupId>com.h2database</groupId>
+    <artifactId>h2</artifactId>
+    <scope>runtime</scope>
+</dependency>
+```
+
+# 
---
+++ src/main/java/com/nimblegeek/demoWebApp/domain/Member.java
@@ -0,0 +1,56 @@
+package com.nimblegeek.demoWebApp.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    public Member() {
+    }
+
+    public Member(long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
